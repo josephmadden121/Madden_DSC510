43d4f9f8d685f1ecda8e6f3c4a94b30eed0c5b23
josephmadden121 <jomadden@my365.bellevue.edu>
2022-09-12 22:05:18 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-print('Hello World')
+# DSC510
+# Week 3
+# 3.1 Programming Assignment
+# Author Joseph Madden
+# 09/12/2022
 
+# Get Name, Company and Cable Length Required
+name = input('Name?\n')
+print("Welcome to the OPTIMA - the fiber optic cable installation company")  # welcome message
+company = input('Please enter your company name: ')
+length = float(input('Please enter how many feet of Fiber Optic Cable you require:'))
 
-
-
-
-
-
-
-
-
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# compute the cost of fiber optic cable and bulk discount
+if 100 < 'cable_length' <= 250:
+    'calculated_cost = round(0.80 * cable_length, 2)
+elif 250 < cable_length <= 500:
+     'calculated_cost' = round(0.70 * cable_length, 2)
+elif 'cable_length' 500:
+        calculated_cost = round(0.50 * cable_length, 2)
+else:
+    calculated_cost: string = 'total_cost'
+# Create Receipt with all the details on it
+print("\nThe Receipt for fiber optic cable installation cost:")
+print("Company Name: ", company_name)
+print("Length of cable to be installed: ", cable_length, "ft")
+print("Total Cost: $", total_cost)
+'if_cable_length > 100:'
+print("Final Cost: $", calculated_cost, "Discount given since length of cable is more than 100ft")
+'else'
+print("Final Cost: $", calculated_cost, "No discount provided since cable length is less than or equal to 100ft")
+print("Our Discounts are:\nup to 100ft: No Discount \n101ft-250ft: $0.80/ft \n251ft-500ft: $0.70/ft \n501ft & More: $0.50/ft")
+print("\nThank you from OPTIMA")
+print('*************** THANK YOU FOR YOUR BUSINESS!!! ***************')
