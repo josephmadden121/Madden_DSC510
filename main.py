f3631b456a48959af11c3c3eb3b249da8d457bc6
josephmadden121 <jomadden@my365.bellevue.edu>
2022-09-27 21:40:04 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 # DSC510
-# Week 4
-# Programming Assignment - 4.1
+# Week 5
+# Programming Assignment - 5.1
 # Author: Joseph Madden
-# Date: 09/20/2022
-# The purpose of this program is to create function to calculate the cable installation cost.
+# Date: 09/27/2022
+# The purpose of this program is to perform different math operating using loops and functions.
 
-# declare the variable for base cost
 base_cost = 0.87
-print("Welcome to the OPTIMA - the fiber optic cable installation company")  # welcome message
-company_name = input("Please tell us your company name: \n")  # save the company name given by user
+# declare and initialize a variable for calculated_cost to remove the undefined error
+calculated_cost = 0
+print("Welcome to my Basic Calculator - the fiber optic cable installation company")
+name = input("What is your name: \n")
+print('')
+print('Hi', name, 'I am happy to help you!')
+#function to calculate addition, subtraction, multiplication and division
+def perform_calculation('num'):
 try:
-    cable_length = float(input("Now tell us length of cable to be installed in feet: \n"))  # save the cable length
-    total_cost = round(base_cost * cable_length, 2)  # round the total cost to 2 digits
+    value1 = float(input('Please enter first number:\n'))
+    value2 = float(input('Please enter second number:\n'))
+    if 'num' == 1:
+        calc = value1 + value2
+        print("Addition of given numbers", value1, "&", value2, "is :", calculated_cost)
+    elif 'num' == 2:
+        calc = value1 - value2
+        print("Subtraction of given numbers", value1, "&", value2, "is :", calculated_cost)
+    elif 'num' == 3:
+        calc = value1 * value2
+        print("multiplication of given numbers", value1,  "&", value2, "is :", calculated_cost)
+    elif 'num' == 4:
+        if value2 != 0:
+            calc = round(value1 / value2, 2)
+            print("Division of given numbers", value1, "&", value2, "is :", calculated_cost)
+        else:
+            print('Please provide second non-zero number')
+except Exception as ex:
+    print('Error occurred: ', ex)
 
-    # function to calculate the cost of cable based on cable length and discounted cost.
-    def cost_calculator(cable_len, 'cst'):
-        final_cost = round(cost*cable_len, 2)
-        return final_cost
+    #function to calculate average of given numbers
+    def calculate_average():
+        try:
+            max_value = int(input('How many numbers you wish to enter\n'))
+            count = 0
+            total = 0
+            if max_value > 0:
+                for value in range(max_value):
+                    print('Please enter number', value + 1, ":")
+                    number = float(input())
+                    count = count + 1
+                    total = total + number
+                    print('Average of given numbers is: ', round(total / count, 2))
+                else:
+                    print('Please enter valid non-zero number')
+        except 'exception' as 'ex':
+            print('Error occurred: ', ex)
 
-    if 100 < cable_length <= 250:
-        cost = 0.80
-        calculated_cost = cost_calculator(cable_length, 'cst')
-    elif 250 < cable_length <= 500:
-        cost = 0.70
-        calculated_cost = cost_calculator(cable_length, 'cst')
-    elif cable_length > 500:
-        cost = 0.50
-        calculated_cost = cost_calculator(cable_length, 'cst')
-    elif 0 < cable_length <= 100:
-        cost = base_cost
-        calculated_cost = cost_calculator(cable_length, 'cst')
-    else:
-        print("\nPlease provide non-zero cable length to calculate the cost")
-    # Create Receipt with all the details on it
-    print("\nThe Receipt for fiber optic cable installation cost:")
-    print("Company Name: ", company_name)
-    print("Length of cable to be installed: ", cable_length, "ft")
-    if cable_length > 100:
-        print("Total Cost: $", total_cost)
-        print("Final Cost: $",calculated_cost, "Discount given since length of cable is more than 100ft")
-    elif 0 < cable_length <= 100:
-        print("Total Cost: $", total_cost)
-        print("Final Cost: $",calculated_cost, "No discount provided since cable length is less than or equal to 100ft")
-    print("Our Discounts are based on length of cable:\nup to 100ft: No Discount \n101ft-250ft: $0.80/ft \n251ft-500ft: $0.70/ft \n501ft & More: $0.50/ft")
-    print("\nThank you from OPTIMA")
-except ValueError as error:
-    print("Please provide valid cable length in feet")
-    print("Error Received: ", error)
-except Exception as e:
-    print("Error Received: ", e)
+#main section of program where user input asked for math operations
+
+    user_input = 'Yes'
+    try:
+        while user_input = input("Would like to perorm math operations? enter Yes or No\n")
+        if user_input.lower() == 'Yes':
+            user_choice = int(input(
+                'Please enter the appropriate number 1:Addition, 2:Subtraction, 3:Multiplication, 4:Division, 5:Average\n'))
+            if user_choice in [1, 2, 3, 4]:
+               perform_calculation('choice')
+            elif user_choice == 5:
+                calculate_average()
+            else:
+                print('Please enter valid number from given options')
+            'else_keyword'
+            print('Thank you!')
+    except Exception as ex:
+        print('Error occurred: ', ex)
+
