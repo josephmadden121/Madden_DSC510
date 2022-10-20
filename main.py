6391ecc62e0cbe6cb4586a868b080740ef2a51a2
josephmadden121 <jomadden@my365.bellevue.edu>
2022-10-19 20:50:12 -0500

    Week 8.1 assignment

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
 # DSC510
-# Week 6
-# Programming Assignment - 6.1
+# Week 8
+# Programming Assignment - 8.1
 # Author: Joseph Madden
-# Date: 10/05/2022
-# The purpose of this program is find the largest and smallest values from the list.
+# Date: 10/19/2022
+# The purpose of this program is being able for me to read the file and display the frequency of the words from the file
 
-# function to sort the given list in descending order
-def sort_list(unsorted_list):
-    for i in range(len(unsorted_list)):
-        for j in range(i+1, len(unsorted_list)):
-            if unsorted_list[i] < unsorted_list[j]:
-                unsorted_list[i], unsorted_list[j] = unsorted_list[j], unsorted_list[i]
-    return unsorted_list
+import string
+
+# function to add word to dictionary and update the count
 
 
-# input for temperatures
-temperatures = [21, 30, 33, 35, 39, 41, 32, 36, 31, 45, 47, 44]
-# empty output list
-output_list =[] for temperature in temperatures:
- if temperatures not in range (0,13):
-  output_list = output_list + [temperatures]
-# print the output_list
-# print (output_list)
-sentinel = 0
-count = 0
-# largest_temp = 47
-# smallest_temp = 21
-message = 'Please enter the temperature, 0 to stop\n'
-try:
-    temp = float(input(message))
-    while temp != sentinel:
-        temperatures.append(temp)
-        count = count + 1
-        temp = float(input(message))
-except Exception as ex:
-    print('Error Received, ', ex)
-    print('Thank you!!')
-if count > 0:
-    print('The list of entered temperatures is: ', temperatures)
-    print('The number of entered temperatures are: ', count)
+def add_word(word, word_dict):
+    if word not in word_dict:
+        word_dict[word] = 1
+    else:
+        word_dict[word] += 1
 
-    # sorted_temp = sorted(temperatures, reverse=True)# save the sorted list in descending order using built-in sorted()
-    # function
+# function to remove punctuations, space from all the lines
 
-    # since this is assignment so created function rather using built-in function to sort list.
-    sorted_temp = sort_list(temperatures)   # save the sorted list in descending order from the function
-    print('The largest temperature from the entered list is: ', sorted_temp[0])
-    print('The smallest temperature from the entered list is: ', sorted_temp[-1])
-else:
-    print('Please enter valid temperature')
+
+def process_line(line, word_dict):
+    line = line.translate(line.maketrans('', '', string.punctuation))
+    line = line.strip()
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        add_word(word, word_dict)
+
+# function to print the length of dictionary and the word frequency
+
+
+def pretty_print(word_dict):
+    word, count = 'Word', 'Count'
+    print(f'\n{word:<20}{count}')
+    print('-------------------------')
+    for key, val in word_dict.items():
+        print(f'{key:<20}{val}')
+
+# function to open the file
+
+
+def main():
+    word_dict = {}
+    try:
+        with open('Gettysburg.txt') as gba_file:
+            for line in gba_file:
+                process_line(line, word_dict)
+        word_dict = dict(sorted(word_dict.items(), key=lambda x: x[1], reverse=True))  # sort the dictionary by count
+        print('Length of the dictionary:', len(word_dict))
+        pretty_print(word_dict)
+    except Exception as ex:
+        print('Error Occurred while opening file: ', ex)
+
+
+if __name__ == '__main__':
+    main()
